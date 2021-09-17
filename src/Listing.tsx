533ac4a2c32682408034fae04c0f4cdf83a5a8b0
your_username <your_email@gmail.com>
2021-09-17 20:46:18 +0200

    Upload testing

--- src/Listing.tsx
+++ src/Listing.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Foo = class extends React.PureComponent {
   return() {
     const { title } = this.context;
-    return <div>{this.context.title}</div>;
+    return <div>{this.context}</div>;
   }
 };
 
